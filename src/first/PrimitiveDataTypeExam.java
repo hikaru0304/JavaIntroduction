6032d7d8b54c0062c7258870b9f139853dee913d
Dayeong Kim <w2002@e-mirim.hs.kr>
2022-02-23 17:43:11 +0900

    first commit

---
+++ src/first/PrimitiveDataTypeExam.java
@@ -0,0 +1,22 @@
+package first;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isFun = false;
+		System.out.println(isFun);
+		
+		char c = 'f';
+		
+		int x = 59;
+		
+		long bing = 345455433L;
+		
+		float f = 32.4f;
+		
+		double d = 3434343.5;
+		
+	}
+
+}
---
+++ src/first/VariableExam.java
@@ -0,0 +1,20 @@
+package first;
+
+public class VariableExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count;
+		count = 10;
+		count = 20;
+		
+		System.out.println(count);
+//		count = 11.1
+		double avg = 11.1;
+		String name ="carami";
+		
+		int totalCount;
+		
+	}
+
+}
