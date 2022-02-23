42c96e588a8190b3625b3c46e594a6e45ff577f2
Dayeong Kim <w2002@e-mirim.hs.kr>
2022-02-23 17:53:39 +0900

    second commit

---
+++ src/first/OperatorExam4.java
@@ -0,0 +1,24 @@
+package first;
+
+public class OperatorExam4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		int j = 10;
+		
+		System.out.println(i == j);
+		System.out.println(i != j);
+		System.out.println(i < j);
+		System.out.println(i <= j);
+		System.out.println(i > j);
+		System.out.println(i >= j);
+		
+		i += 10;
+		System.out.println(i);
+		
+		System.out.println(i -= 5);
+		System.out.println(i);
+	}
+
+}
