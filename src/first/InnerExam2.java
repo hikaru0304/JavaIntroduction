18d877c6e46d5e1e867a6afb0ffbf9e116597ea5
Dayeong Kim <w2002@e-mirim.hs.kr>
2022-02-25 04:06:53 +0900

    열두번째 커밋

---
+++ src/first/InnerExam2.java
@@ -0,0 +1,17 @@
+package first;
+
+public class InnerExam2 {
+	static class Cal{
+		int value = 0;
+		public void plus() {
+			value++;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InnerExam2.Cal cal = new InnerExam2.Cal();
+		cal.plus();
+		System.out.println(cal.value);
+	}
+
+}
