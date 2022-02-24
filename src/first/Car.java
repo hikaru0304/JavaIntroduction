cbc54058ff86a8bdff6244da2568934069b6058e
Dayeong Kim <w2002@e-mirim.hs.kr>
2022-02-25 01:31:53 +0900

    sixth commit

---
+++ src/first/Car.java
@@ -0,0 +1,6 @@
+package first;
+
+public class Car {
+	String name;
+	int number;
+}
---
+++ src/first/ReferenceDataType.java
@@ -0,0 +1,12 @@
+package first;
+
+public class ReferenceDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 4;
+		
+		String str = new String("hello");
+	}
+
+}
